e3a4d9c9b26908a0ff4703e480133fff79ade064
Huy Nhat Mai <nhathuylk0123@gmail.com>
2024-10-23 03:09:16 +1100

    Modification and add shapes function

--- application/toolbar.py
+++ application/toolbar.py
@@ -2,12 +2,22 @@ import tkinter as tk
 from tkinter import colorchooser
 
 class Toolbar(tk.Frame):
-    def __init__(self, master=None, set_color=None, clear_space=None, set_eraser=None):
+    def __init__(self, master=None, 
+                 set_color=None, 
+                 clear_space=None, 
+                 set_eraser=None, 
+                 set_shape=None,
+                 normal_drawing=None):
         super().__init__(master)
+
         self.master = master
         self.set_color = set_color
         self.clear_space = clear_space
         self.set_eraser = set_eraser
+        self.set_shape = set_shape
+        self.shapes = ["Line", "Rectangle", "Circle"]
+        self.selected_shape = tk.StringVar(value=None)
+        self.normal_drawing = normal_drawing
         self.create_widgets()
         self.pack()
 
@@ -19,19 +29,60 @@ class Toolbar(tk.Frame):
         self.color_label = tk.Label(self, bg="black", width=4, height = 2, relief="sunken")
         self.color_label.grid(row=0, column=1, padx=5)
 
-        self.clear_button = tk.Button(self, text="Clear", command=self.reset_clear_space)
-        self.clear_button.grid(row=0, column=2, padx=5)
-
         self.eraser = tk.PhotoImage(file="assets/eraser.png").subsample(14)
-        self.eraser_button = tk.Button(self, image=self.eraser, command=self.set_eraser)
-        self.eraser_button.grid(row=0, column=3, padx=5)
+        self.eraser_button = tk.Button(self, image=self.eraser, command=self.erase_mode)
+        self.eraser_button.grid(row=0, column=2, padx=5)
+
+        self.pen = tk.PhotoImage(file="assets/pencil.png").subsample(14)
+        self.pen_button = tk.Button(self, image=self.pen, command=self.normal_mode)
+        self.pen_button.grid(row=0, column=3, padx=5)
+
+        self.shapes_image = {
+            "Line": tk.PhotoImage(file="assets/line.png").subsample(14),
+            "Rectangle": tk.PhotoImage(file="assets/rectangle.png").subsample(14),
+            "Circle": tk.PhotoImage(file="assets/circle.png").subsample(14)
+        }
+
+        self.shapes_icon = tk.PhotoImage(file="assets/shapes.png").subsample(14)
+        self.shapes_menu = tk.Menubutton(self, image=self.shapes_icon, indicatoron=True, borderwidth=1, relief="raised")
+        self.menu = tk.Menu(self.shapes_menu, tearoff=False)
+        self.shapes_menu.configure(menu=self.menu)
+
+        for shape in self.shapes:
+            self.menu.add_radiobutton(label=shape, variable=self.selected_shape, value=shape, image=self.shapes_image[shape], compound="left", command=self.choose_shape)
+        
+        self.shapes_menu.grid(row=0, column=4, padx=5)
+
+        self.clear_button = tk.Button(self, text="Clear", command=self.reset_clear_space)
+        self.clear_button.grid(row=0, column=7, padx=5)
 
     def choose_color(self, color = "black"):
         color = colorchooser.askcolor(title="Choose a color")
         if color:
             self.color_label.config(background=str(color[1]))
             self.set_color(color[1])
-    
+
+    def choose_shape(self):
+        selected_shape = self.selected_shape.get()
+        if selected_shape:  # Only update if a shape is selected
+            self.shapes_menu.config(image=self.shapes_image[selected_shape])
+            self.set_shape(selected_shape)
+
+    def normal_mode(self):
+        self.normal_drawing()
+        self.shapes_menu.config(image=self.shapes_icon)
+        self.set_shape(None)
+        self.selected_shape.set(None)  # Clear the selection of the shape
+
     def reset_clear_space(self):
         self.clear_space()
         self.color_label.config(background="black")
+        self.shapes_menu.config(image=self.shapes_icon)
+        self.set_shape(None)
+        self.selected_shape.set(None)  # Clear the selection of the shape
+
+    def erase_mode(self):
+        self.set_eraser()
+        self.set_shape(None)
+        self.selected_shape.set(None)  # Clear the selection of the shape
+        self.shapes_menu.config(image=self.shapes_icon)
